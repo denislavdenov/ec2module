2918f8e5acb379dc617fe025556a871836b769a0
Denislav Denov <denislavdenov@Denislavs-MacBook-Pro.local>
2018-10-11 18:03:31 +0300

    add code

--- servers/servers.tf
+++ servers/servers.tf
@@ -23,8 +23,10 @@ resource "aws_instance" "example" {
   key_name               = "${aws_key_pair.training.id}"
   vpc_security_group_ids = ["${var.security_group_id}"]
 
-  provisioner "local-exec" {
-    command = "echo Hello"
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get -y update"
+    ]
   }
 
   connection {
